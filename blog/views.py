728d07bb2b0b34bcb6637c88930890ef1e523624
susan <shuxin1@ualberta.ca>
2020-02-19 21:52:04 -0700

    Added views for creating new posts and editing existing posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post # name of the model imported - Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -11,3 +12,34 @@ def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
 
+def post_new(request):
+    # when we submit the form, we are brought back to the same view,
+    # but this time we have some more data in request
+    # more specifically in request.POST ("posting" data)
+    # in HTML we have a method called "post"
+    # all fields from the form are now in request.POST
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
